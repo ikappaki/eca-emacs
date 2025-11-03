d95c565f4e620df124331160c59f385e81992d5c
ikappaki <ikappaki@users.noreply.github.com>
2025-11-03 20:50:19 +0000

    poller

--- test/eca-diff-test.el
+++ test/eca-diff-test.el
@@ -8,6 +8,19 @@
     "\n")
   "Native end-of-line string for the current operating system.")
 
+(defmacro poll-until (condition timeout-secs)
+  "Poll every 0.2 secs until CONDITION becomes true or error out if TIMEOUT-SECS elapses."
+  (let* ((interval-secs 0.2)
+         (count (truncate (/ timeout-secs interval-secs))))
+    `(cl-loop repeat ,count
+              for condition = ,condition
+              if condition
+                return condition
+              else
+                do (sleep-for ,interval-secs)
+              finally (error ":cider-tests-poll-until-errored :timed-out-after-secs %d :waiting-for %S"
+                             ,timeout-secs (quote ,condition)))))
+
 (describe "eca-diff-show-ediff"
           :var ((insert-orig (symbol-function 'insert))
                 (contents '()))
@@ -30,6 +43,7 @@
 
                 ;; We expect this function to call `insert` twice.
                 (eca-diff-show-ediff path diff)
+                (poll-until (> (length contents) 1) 5)
                 (expect (reverse contents)
                         :to-equal
                         `((,(format "*eca-diff-orig:%s*" path) "Line 1
